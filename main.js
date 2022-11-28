03e263a4ab3907c5e6d861869b40372e85d0bb00
Jlance999 <jeremy.perkins99@gmail.com>
2022-11-27 23:00:34 -0500

    Added confirmation page, updated cart, some links, and properly linked main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function show_mens() {
     document.getElementsByClassName("womens").style.display="none";
-}
+};
 
 document.getElementById('womensonly').addEventListener('click',function(){
     document.getElementById("womens").classList.add("d-none");
@@ -8,6 +8,8 @@ document.getElementById('womensonly').addEventListener('click',function(){
 
 function show_womens() {
     document.getElementById("womens").classList.add("d-none");
-}
+};
 
-//
+document.getElementById("confirm").onclick = function () {
+    location.href = "confirmation.html";
+};
