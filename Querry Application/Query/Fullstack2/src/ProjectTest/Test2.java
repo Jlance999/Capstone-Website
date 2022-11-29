9f76b7a0d0aa6ed1c09cc970ab214b9bab54cc5b
Jeremy Perkins <jeremy.perkins99@gmail.com>
2022-11-28 23:06:46 -0500

    Added beginnings of text menu, sorted customer query, started on order history query

--- Querry Application/Query/Fullstack2/src/ProjectTest/Test2.java
+++ Querry Application/Query/Fullstack2/src/ProjectTest/Test2.java
@@ -5,6 +5,7 @@ package ProjectTest;
 	import java.sql.DriverManager;
 	import java.sql.ResultSet;
 	import java.sql.Statement;
+	import java.util.Scanner;
 
 	public class Test2 {
 
@@ -17,24 +18,71 @@ package ProjectTest;
 			//connect with database fullstack with username and its details
 			try (Connection con = DriverManager.getConnection(url, username, pass);
 					Statement statement = con.createStatement();) {
-			// Select statement
+			
+			// Select statement for Customer data
 				Statement statement1;
 	            statement1 = con.createStatement();
 	            ResultSet resultSet;
 	            resultSet = statement1.executeQuery(
-	                "select * from orders");
+	                "select * from Customers");
+			// Building Print Statement for Customer List
 	            int id;
-	            int userId;
-	            int productId;
-	            while (resultSet.next()) {
-	            	id = resultSet.getInt("id");
-	            	userId= resultSet.getInt("userId");
-	            	productId = resultSet.getInt("productId");
-	                System.out.println(" ID : " + id
-	                                   + "   User ID: " +  userId  + " productId  " + productId);
+	        	String userEmail;
+	            String userPass;
+				String fName;
+				String lName;
+				
+			// Select statement for order data
+				Statement statement2;
+	            statement2 = con.createStatement();
+	            ResultSet resultSet2;
+	            resultSet2 = statement2.executeQuery(
+	                "select * from orders");
+	            int id2;
+	            int userId2;
+	            int productId2;
+	            while (resultSet2.next()) {
+	            	id2 = resultSet2.getInt("id");
+	            	userId2= resultSet2.getInt("userId");
+	            	productId2 = resultSet2.getInt("productId");
+	                System.out.println("Order ID : " + id2
+	                                   + "   User ID: " +  userId2  + " productId  " + productId2);
 	            }
+				System.out.println("IT Capstone Group 2 Query Application vers. 1.0");
+				System.out.println("Please press appropriate key to navigate application (Case Sensitive).");
+				System.out.println("Menu:");
+				System.out.println("C) Print list of customers and their login information");
+				System.out.println("O) Print customer order history");
+				System.out.println("Q) Quit");
+
+				String options = null;
+				Scanner scan = new Scanner(System.in); // Capturing the input
+				do {
+					options = scan.nextLine();
+					switch (options) {
+						case "C":
+							System.out.println("Full Customer List with Email/Pass:");
+							while (resultSet.next()) {
+								id = resultSet.getInt("CUS_CODE");
+								userEmail= resultSet.getString("userEmail");
+								userPass = resultSet.getString("userPass");
+								fName = resultSet.getString("CUS_FNAM");
+								lName = resultSet.getString("CUS_LNAME");
+								System.out.println("User Name: " + fName + " " + lName + "  User ID : " + id
+												+ "   User Email: " +  userEmail  + " User Password: " + userPass);
+							}
+							break;
+						case "O":
+							// do what you want
+							break;
+					}
+				} while (!options.equals("Q")); // quitting the program
+			
+
 	            resultSet.close();
 	            statement1.close();
+				resultSet2.close();
+				statement2.close();
 	            con.close();
 			
 			} catch (Exception e) {
@@ -44,5 +92,3 @@ package ProjectTest;
 		}
 
 	}
-
-
